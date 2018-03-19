524bf8917d3ce56d6e04f4e01d845f77feba282a
arvinds <1141191arvind.singh@gmail.com>
2018-03-19 17:47:17 +0530

    Added functionality to check account number

--- library/src/test/java/AccountTest.java
+++ library/src/test/java/AccountTest.java
@@ -1,13 +1,26 @@
 import com.thoughtworks.step.bank.Account;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AccountTest {
+
+    private Account account;
+
+    @Before
+    public void setUp() throws Exception {
+        account = new Account("1234", 1000);
+    }
+
     @Test
-    public void checkBalance(){
-        Account account = new Account("1234", 1000);
-        assertThat(account.getBalance(),is(1000));
+    public void checkBalance() {
+        assertThat(account.getBalance(), is(1000));
+    }
+
+    @Test
+    public void checkAccountNumber() {
+        assertThat(account.getAccountNumber(),is("1234"));
     }
 }
