b4412aac6f86b586a23c94e7a08a2b0c5584e376
arvinds <1141191arvind.singh@gmail.com>
2018-03-19 17:37:27 +0530

    Added functionality to check balance

---
+++ library/src/test/java/AccountTest.java
@@ -0,0 +1,13 @@
+import com.thoughtworks.step.bank.Account;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class AccountTest {
+    @Test
+    public void checkBalance(){
+        Account account = new Account("1234", 1000);
+        assertThat(account.getBalance(),is(1000));
+    }
+}
